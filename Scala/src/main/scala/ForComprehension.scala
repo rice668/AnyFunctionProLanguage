51270c788f8a10106b4b5fe89d1643df432be7a7
mingleizhang <mingleizhang@Ctrip.com>
2016-08-06 20:52:38 +0800

    For

---
+++ Scala/src/main/scala/ForComprehension.scala
@@ -0,0 +1,21 @@
+/**
+  * Created by root on 16-8-6.
+  */
+object ForComprehension extends App {
+  val num = List(1, 2, 3, 4, 5, 6, 7, 8)
+  for (i <- num) // generator expression
+    print(i + " ")
+  println()
+  for (i <- num if i > 3 if i % 2 == 0) // guard and you can add any types here...
+    print(i + " ")
+  println()
+
+  val dogBreeds = List("Bdsc", "sdcds", "IOOPSA", "csdcuiw")
+
+  val filteredBreeds = for {
+    breed <- dogBreeds
+    if breed.contains("csdcuiw")
+  } yield filteredBreeds
+  filteredBreeds.foreach(print)
+
+}
